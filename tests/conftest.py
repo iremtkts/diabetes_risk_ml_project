a99fb0b47ed792aa0ac0d4eb76173f19521cbe25
Irem <tektasirem13@gmail.com>
2026-06-04 21:08:32 +0300

    Feature/tests (#6)
    * feat: add data loading, schema validation and unit tests
    
    * test: add unit tests for validation

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import pytest
+import pandas as pd
 
 from src.config.path import FIXTURES_DIR
 
@@ -18,3 +19,21 @@ def empty_csv_path() -> Path:
 @pytest.fixture
 def unsupported_file_path() -> Path:
     return FIXTURES_DIR / "unsupported.txt"
+
+
+@pytest.fixture
+def valid_dataframe() -> pd.DataFrame:
+
+    return pd.DataFrame(
+        {
+            "Pregnancies": [1, 2],
+            "Glucose": [100, 120],
+            "BloodPressure": [70, 80],
+            "SkinThickness": [20, 30],
+            "Insulin": [80, 100],
+            "BMI": [25.5, 30.1],
+            "DiabetesPedigreeFunction": [0.5, 0.8],
+            "Age": [25, 35],
+            "Outcome": [0, 1],
+        }
+    )
